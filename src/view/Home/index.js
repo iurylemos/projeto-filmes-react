030c07f9d6807d73eeca7d856e6175e8e87bb01d
Iury Lemos <iurylemos10@gmail.com>
2020-01-15 00:04:49 -0300

    Tratamento de página não encontrada

--- src/view/Home/index.js
+++ src/view/Home/index.js
@@ -1,11 +1,50 @@
 import React, { Component } from 'react';
-import './Home.css'
+import './Home.css';
+import { Link } from 'react-router-dom';
 
 class Home extends Component {
+
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      filmes: []
+    }
+
+    this.loadFilmes = this.loadFilmes.bind(this)
+  }
+
+  componentDidMount() {
+    this.loadFilmes()
+  }
+
+  loadFilmes() {
+    //URL da API: https://sujeitoprogramador.com/r-api/?api=filmes/
+    let url = 'https://sujeitoprogramador.com/r-api/?api=filmes';
+    fetch(url).then((r) => {
+      r.json().then((json) => {
+        this.setState({ filmes: json })
+        console.log(json)
+      })
+    })
+  }
+
   render() {
     return (
-      <div>
-        Página Home
+      <div className="container">
+        <div className="lista-filmes">
+          {
+            this.state.filmes.map((item) => {
+              return (
+                <article className="filme" key={item.id}>
+                  <strong>{item.nome}</strong>
+                  <img src={item.foto} alt="Capa" />
+                  <Link to="/">Acessar</Link>
+                </article>
+              )
+            })
+          }
+        </div>
       </div>
     )
   }
