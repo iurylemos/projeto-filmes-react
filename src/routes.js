8e39de1fa64d0db14d3ea7dae22eb0d17401a3d5
Iury Lemos <iurylemos10@gmail.com>
2020-01-15 22:40:20 -0300

    Criando autenticação

--- src/routes.js
+++ src/routes.js
@@ -1,9 +1,21 @@
 import React from 'react';
-import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import { BrowserRouter, Route, Switch, Redirect } from 'react-router-dom';
 import Home from './view/Home';
 import Header from './components/Header'
 import Erro from './view/Erro'
 import Filme from './view/Filme';
+import Painel from './view/Painel';
+import { autenticado } from './auth';
+
+const PrivateRoute = ({ component: Component, ...rest }) => (
+  <Route {...rest} render={props => (
+    autenticado() ? (
+      <Component {...props} />
+    ) : (
+        <Redirect to={{ pathname: '/', state: { from: props.location } }} />
+      )
+  )} />
+)
 
 const Routes = () => {
   return (
@@ -11,6 +23,7 @@ const Routes = () => {
       <Header />
       <Switch>
         <Route exact path="/" component={Home} />
+        <PrivateRoute exact path="/painel" component={Painel} />
         <Route exact path="/filme/:id" component={Filme} />
         <Route path="*" component={Erro} />
       </Switch>
---
+++ src/view/Painel/index.js
@@ -0,0 +1,21 @@
+import React, { Component } from 'react';
+
+class Painel extends Component {
+  constructor(props) {
+    super(props)
+
+    this.state = {
+
+    }
+  }
+
+  render() {
+    return (
+      <div>
+        <h1>Bem vindo você está logado!</h1>
+      </div>
+    )
+  }
+}
+
+export default Painel
